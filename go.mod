5e9771c2dfb0d2072cc4fdbcc444a51aaf454435
Jimmie Johansson <jimmitjoo@gmail.com>
2023-07-07 09:31:30 +0200

    circle done

--- go.mod
+++ go.mod
@@ -5,13 +5,13 @@ go 1.20
 require (
 	github.com/aws/aws-sdk-go v1.44.296
 	github.com/aws/aws-sdk-go-v2 v1.18.1
+	github.com/aws/aws-sdk-go-v2/config v1.18.27
 	github.com/aws/aws-sdk-go-v2/service/s3 v1.36.0
 	github.com/joho/godotenv v1.5.1
 )
 
 require (
 	github.com/aws/aws-sdk-go-v2/aws/protocol/eventstream v1.4.10 // indirect
-	github.com/aws/aws-sdk-go-v2/config v1.18.27 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.13.26 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.13.4 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.34 // indirect
--- main.go
+++ main.go
@@ -2,29 +2,22 @@ package main
 
 import (
 	"bytes"
+	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	v4 "github.com/aws/aws-sdk-go-v2/aws/signer/v4"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/jimmitjoo/bilder/actions"
+	"github.com/joho/godotenv"
 	"io"
 	"log"
 	"net/http"
 	"os"
 	"time"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/joho/godotenv"
 )
 
-var (
-	srcKey      = "inputs/asdsad.jpeg"
-	destKey     = "hejsan.jpg"
-	autotoneURL = "https://image.adobe.io/lrService/autoTone"
-)
-
-type CutoutPayload struct {
+type AdobePayload struct {
 	Input struct {
 		Storage string `json:"storage"`
 		Href    string `json:"href"`
@@ -38,6 +31,28 @@ type CutoutPayload struct {
 	} `json:"output"`
 }
 
+type RemoveBackgroundPayload struct {
+	Input struct {
+		Href    string `json:"href"`
+		Storage string `json:"storage"`
+	} `json:"input"`
+	Output struct {
+		Href    string `json:"href"`
+		Storage string `json:"storage"`
+		Mask    struct {
+			Format string `json:"format"`
+		} `json:"mask"`
+	} `json:"output"`
+}
+
+var (
+	region              = os.Getenv("AWS_REGION")
+	srcKey              = "inputs/asdsad.jpeg"
+	destKey             = "outputs/tjenaberra.jpg"
+	autotoneURL         = "https://image.adobe.io/lrService/autoTone"
+	removeBackgroundURL = "https://image.adobe.io/sensei/cutout"
+)
+
 func main() {
 	err := godotenv.Load()
 	if err != nil {
@@ -46,37 +61,29 @@ func main() {
 
 	clientID := os.Getenv("ADOBE_CLIENT_ID")
 	token := os.Getenv("ADOBE_ACCESS_TOKEN")
-	region := os.Getenv("AWS_REGION")
+
 	bucket := os.Getenv("AWS_BUCKET")
-	s3AccessKeyID := os.Getenv("AWS_S3_ACCESS_KEY")
-	s3SecretAccessKey := os.Getenv("AWS_S3_SECRET_ACCESS_KEY")
+	//s3AccessKeyID := os.Getenv("AWS_S3_ACCESS_KEY")
+	//s3SecretAccessKey := os.Getenv("AWS_S3_SECRET_ACCESS_KEY")
 
-	fmt.Println("Client ID:", clientID)
-	fmt.Println("Token:", token)
-	fmt.Println("Region:", region)
-	fmt.Println("Bucket:", bucket)
-	fmt.Println("SrcKey:", srcKey)
-	fmt.Println("DestKey:", destKey)
-	fmt.Println("S3AccessKeyID:", s3AccessKeyID)
-	fmt.Println("S3SecretAccessKey:", s3SecretAccessKey)
+	cfg, err := config.LoadDefaultConfig(context.TODO(),
+		config.WithRegion(region),
+	)
+	if err != nil {
+		fmt.Println("Failed to load AWS configuration:", err)
+		return
+	}
 
-	/*
-		fmt.Println(clientID)
-		fmt.Println(token)
-		fmt.Println(region)
-		fmt.Println(bucket)
-		fmt.Println(srcKey)
-		fmt.Println(destKey)
-		fmt.Println(s3AccessKeyID)
-		fmt.Println(s3SecretAccessKey)
-	*/
+	s3Client := s3.NewFromConfig(cfg)
+	presignClient := s3.NewPresignClient(s3Client)
 
-	// Get pre-signed URLs
-	inputSrcURL := getPresignedURL(bucket, srcKey, "getObject", 3600, region, s3AccessKeyID, s3SecretAccessKey)
-	outputDestURL := getPresignedURL(bucket, destKey, "putObject", 3600, region, s3AccessKeyID, s3SecretAccessKey)
+	presigner := actions.Presigner{
+		PresignClient: presignClient,
+	}
 
-	// fmt.Println(inputSrcURL)
-	fmt.Println(outputDestURL)
+	inputSrcSigned, _ := presigner.GetObject(bucket, srcKey, 1000)
+	outputDestSigned, _ := presigner.PutObject(bucket, destKey, 1000)
+	outputLinkSigned, _ := presigner.GetObject(bucket, destKey, 1000)
 
 	// Create HTTP headers
 	headers := map[string]string{
@@ -85,15 +92,118 @@ func main() {
 		"Content-Type":  "application/json",
 	}
 
+	// Ask user what action to take
+	fmt.Println("What action do you want to take?")
+	fmt.Println("1. Remove background")
+	fmt.Println("2. Auto tone")
+	var action int
+	fmt.Scanln(&action)
+
+	switch action {
+	case 1:
+		// Invoke removeBackground API
+		resp, err := removeBackground(removeBackgroundURL, headers, inputSrcSigned, outputDestSigned)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		// Poll status
+		err = pollStatus(resp, headers)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println(resp)
+
+	case 2:
+		// Invoke autoTone API
+		resp, err := invokeAutotone(autotoneURL, headers, inputSrcSigned, outputDestSigned)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		// Poll status
+		err = pollStatus(resp, headers)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		fmt.Println(resp)
+
+	default:
+		fmt.Println("Invalid action")
+	}
+
+	fmt.Println("Output link:", outputLinkSigned.URL)
+
+}
+
+func removeBackground(url string, headers map[string]string, inputSrcSigned *v4.PresignedHTTPRequest, outputDestSigned *v4.PresignedHTTPRequest) (*http.Response, error) {
+
 	// Create payload
+	payload := RemoveBackgroundPayload{
+		Input: struct {
+			Href    string `json:"href"`
+			Storage string `json:"storage"`
+		}{
+			Href:    inputSrcSigned.URL,
+			Storage: "external",
+		},
+		Output: struct {
+			Href    string `json:"href"`
+			Storage string `json:"storage"`
+			Mask    struct {
+				Format string `json:"format"`
+			} `json:"mask"`
+		}{
+			Href:    outputDestSigned.URL,
+			Storage: "external",
+			Mask: struct {
+				Format string `json:"format"`
+			}{
+				Format: "soft",
+			},
+		},
+	}
+
+	payloadJSON, err := json.Marshal(payload)
+	if err != nil {
+		fmt.Println(err)
+		return nil, nil
+	}
+
+	fmt.Println("Payload:", string(payloadJSON))
+
+	jsonPayload, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonPayload))
+	if err != nil {
+		return nil, err
+	}
+
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
+
+	client := &http.Client{}
+	return client.Do(req)
+}
+
+func invokeAutotone(url string, headers map[string]string, inputSrcSigned *v4.PresignedHTTPRequest, outputDestSigned *v4.PresignedHTTPRequest) (*http.Response, error) {
+	// Create payload based on AdobePayload struct
 	payload := map[string]interface{}{
 		"inputs": map[string]interface{}{
-			"href":    inputSrcURL,
+			"href":    inputSrcSigned.URL,
 			"storage": "external",
 		},
 		"outputs": []map[string]interface{}{
 			{
-				"href":    outputDestURL,
+				"href":    outputDestSigned.URL,
 				"storage": "external",
 				"type":    "image/jpeg",
 			},
@@ -103,67 +213,11 @@ func main() {
 	payloadJSON, err := json.Marshal(payload)
 	if err != nil {
 		fmt.Println(err)
-		return
+		return nil, nil
 	}
 
 	fmt.Println("Payload:", string(payloadJSON))
 
-	// Invoke autoTone API
-	resp, err := invokeAutotone(autotoneURL, headers, payload)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	// Poll status
-	err = pollStatus(resp, headers)
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	fmt.Println(resp)
-}
-
-func getPresignedURL(bucket, key, operation string, expiresIn int64, region string, s3AccessKeyID string, s3SecretAccessKey string) string {
-	sess := session.Must(session.NewSession(&aws.Config{
-		Region: aws.String(region),
-		Credentials: credentials.NewStaticCredentials(
-			s3AccessKeyID,
-			s3SecretAccessKey,
-			"",
-		),
-	}))
-
-	uploader := s3manager.NewUploader(sess)
-
-	if operation == "PutObject" {
-		// Generate a pre-signed URL for a PutObject operation
-		uploadUrl, _ := uploader.Upload(&s3manager.UploadInput{
-			Bucket: aws.String(bucket),
-			Key:    aws.String(key),
-			ACL:    aws.String("bucket-owner-full-control"),
-		})
-
-		return uploadUrl.Location
-	}
-
-	// Generate a pre-signed URL for a GetObject operation
-	req, _ := uploader.S3.GetObjectRequest(&s3.GetObjectInput{
-		Bucket: aws.String(bucket),
-		Key:    aws.String(key),
-	})
-
-	getUrl, err := req.Presign(time.Duration(expiresIn) * time.Second)
-	if err != nil {
-		fmt.Println(err)
-		return ""
-	}
-
-	return getUrl
-}
-
-func invokeAutotone(url string, headers map[string]string, payload map[string]interface{}) (*http.Response, error) {
 	jsonPayload, err := json.Marshal(payload)
 	if err != nil {
 		return nil, err
@@ -209,6 +263,7 @@ func pollStatus(resp *http.Response, headers map[string]string) error {
 		return err
 	}
 
+	fmt.Println("Headers:", resp.Header)
 	fmt.Println("Response Body:", string(body))
 
 	var tmp map[string]interface{}
@@ -256,38 +311,94 @@ func pollStatus(resp *http.Response, headers map[string]string) error {
 			return fmt.Errorf("API Error: %v", tmpStatus)
 		}
 
-		outputs, ok := tmpStatus["outputs"].([]interface{})
-		if !ok || len(outputs) == 0 {
+		var outputs []interface{}
+		var output map[string]interface{}
+
+		if tmpOutputs, ok := tmpStatus["outputs"].([]interface{}); ok && len(tmpOutputs) > 0 {
+			// Use tmpOutputs as the value for outputs
+			outputs = tmpOutputs
+		} else if tmpOutput, ok := tmpStatus["output"].(map[string]interface{}); ok {
+			// Use tmpOutput as the value for output
+			output = tmpOutput
+		} else {
 			return fmt.Errorf("invalid outputs data")
 		}
 
-		output, ok := outputs[0].(map[string]interface{})
-		if !ok || output == nil {
-			return fmt.Errorf("invalid output data")
+		if len(outputs) > 0 {
+			// Use outputs[0] as the value for output
+			if tmpOutput, ok := outputs[0].(map[string]interface{}); ok {
+				output = tmpOutput
+			} else {
+				return fmt.Errorf("invalid output data")
+			}
 		}
 
 		fmt.Println("Output:", output)
 
-		status, ok := output["status"].(string)
-		if !ok || status == "" {
-			return fmt.Errorf("invalid status data")
-		}
-
-		fmt.Println("Status:", status)
-
-		if status == "failed" {
-			if _, errorExists := output["error"]; errorExists {
-				errorValue, ok := output["error"].(string)
-				if !ok {
-					return fmt.Errorf("invalid error data")
+		if tmpStatus["status"] != nil {
+			status, ok := tmpStatus["status"].(string)
+			if !ok || status == "" {
+				// If status is not found, check for error
+				if _, errorExists := tmpStatus["error"]; errorExists {
+					errorValue, ok := tmpStatus["error"].(string)
+					if !ok {
+						return fmt.Errorf("invalid error data")
+					}
+					fmt.Println("Error:", errorValue)
 				}
-				fmt.Println("Error:", errorValue)
-			}
-			return nil
-		}
 
-		if status == "succeeded" {
-			return nil
+				return fmt.Errorf("invalid status data")
+			}
+
+			fmt.Println("Status:", status)
+
+			if status == "failed" {
+				if _, errorExists := tmpStatus["error"]; errorExists {
+					errorValue, ok := tmpStatus["error"].(string)
+					if !ok {
+						return fmt.Errorf("invalid error data")
+					}
+					fmt.Println("Error:", errorValue)
+				}
+				return nil
+			}
+
+			if status == "succeeded" {
+				return nil
+			}
+		} else {
+
+			status, ok := output["status"].(string)
+			if !ok || status == "" {
+				// If status is not found, check for error
+				if _, errorExists := output["error"]; errorExists {
+					errorValue, ok := output["error"].(string)
+					if !ok {
+						return fmt.Errorf("invalid error data")
+					}
+					fmt.Println("Error:", errorValue)
+				}
+
+				return fmt.Errorf("invalid status data")
+			}
+
+			fmt.Println("Status:", status)
+
+			if status == "failed" {
+				if _, errorExists := output["error"]; errorExists {
+					errorValue, ok := output["error"].(string)
+					if !ok {
+						return fmt.Errorf("invalid error data")
+					}
+					fmt.Println("Error:", errorValue)
+				}
+				return nil
+			}
+
+			if status == "succeeded" {
+				return nil
+			}
+
 		}
 
 		time.Sleep(1 * time.Second)
